94ad076fb4a60c7c7b722cbe851cfe3f4285f706
YRB <2576219848@qq.com>
2021-01-26 17:47:03 +0800

    基础项的新增,删除和修改功能

--- src/components/page/BaseProject.vue
+++ src/components/page/BaseProject.vue
@@ -57,19 +57,28 @@
                           <el-table-column align="center" label="操作" width="180">
                             <template slot-scope="scope" >
                                 <el-button type="primary" icon="el-icon-edit" size="mini" @click="modify(scope.row)"></el-button>
-                                <el-button type="danger"  icon="el-icon-delete" size="mini" @click="deleted(scope.row)"></el-button>
+                                <el-popconfirm title="确认删除当前项目以及子项目？" @confirm="deleted(scope.row)">
+                                    <el-button type="danger"  icon="el-icon-delete" size="mini" slot="reference" ></el-button>
+                                </el-popconfirm>
                             </template>
                           </el-table-column>
                         </el-table>
                         <br>
                         <div>
-                            <el-pagination :total="total" :current-page="currentPage" :page-size="pageSize" :page-sizes="[5,10,15,20]" @size-change="handleSizeChange" @current-change="handleCurrentChange" layout="total,sizes,prev,pager,next,jumper"></el-pagination>
+                            <el-pagination 
+                            :total="total" 
+                            :current-page="currentPage" 
+                            :page-size="pageSize" 
+                            :page-sizes="[5,10,15,20]" 
+                            @size-change="handleSizeChange" 
+                            @current-change="handleCurrentChange" 
+                            layout="total,sizes,prev,pager,next,jumper">
+                            </el-pagination>
                         </div>
                     </div>
                 </el-card>
             </el-main>
         </el-container>
-
 
         <el-dialog title="添加基础项目" :visible.sync="addProject">
         <el-form :model="addForm" label-position="left" label-width="80px">
@@ -78,13 +87,16 @@
                     :options="parentNode"
                     :props="addProjectProps"
                     :show-all-levels="false"
-                    clearable></el-cascader>
+                    clearable
+                    filterable
+                    @change="addParentChange"
+                    ></el-cascader>
             </el-form-item>
             <el-form-item label="名称" >
                 <el-input v-model="addForm.name" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item label="等级" >
-                <el-select v-model="addForm.level" placeholder="请选择活动区域">
+                <el-select v-model="addForm.level" placeholder="请选择级别">
                     <el-option label="无级别" value="0"></el-option>
                     <el-option label="一般" value="1"></el-option>
                     <el-option label="严重" value="2"></el-option>
@@ -95,8 +107,30 @@
             </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
-            <el-button @click="concelAdd">取 消</el-button>
-            <el-button type="primary" @click="addProject = false">确 定</el-button>
+            <el-button @click="cancelAdd">取 消</el-button>
+            <el-button type="primary" @click="ensureAddProject">确 定</el-button>
+        </div>
+        </el-dialog>
+
+        <el-dialog title="更新基础项" :visible.sync="updateProject">
+            <el-form :model="updateForm" label-position="left" label-width="80px">
+            <el-form-item label="名称" >
+                <el-input v-model="updateForm.name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="等级" >
+                <el-select v-model="updateForm.level" placeholder="请选择要更改的级别">
+                    <el-option label="无级别" value="0"></el-option>
+                    <el-option label="一般" value="1"></el-option>
+                    <el-option label="严重" value="2"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="分值" >
+                <el-input v-model="updateForm.score" autocomplete="off"></el-input>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="updateProject = false">取 消</el-button>
+            <el-button type="primary" @click="ensureUpdateProject">确 定</el-button>
         </div>
         </el-dialog>
     </div>
@@ -106,17 +140,21 @@
 import Axios from 'axios';
 export default {
     name: 'BaseProject',
-    components: {  },
-    directives: {  },
     data() {
         return {
+            updateProject: false,
+            updateForm: {
+                id: 0,
+                name: '',
+                level: undefined,
+                score: 0
+            },
             addProject: false,
             addProjectProps: {
                 label: 'name',  
                 value: 'id',
                 checkStrictly: true,
-                expandTrigger: 'hover',
-
+                expandTrigger: 'click',
             },
             nodeHasClick: false,
             isSelect: false,
@@ -133,9 +171,9 @@ export default {
                 label: 'name'
             },
             addForm: {
-                parentProject: {},
+                parentId: 0,
                 name: '',
-                level: 0,
+                level: "",
                 score: 0
             },
             parentNode:[],//添加模板时的父级节点
@@ -171,28 +209,65 @@ export default {
                     return Array.isArray(temp) && temp.length !== 0 ? temp.slice(this.start,this.end) : [];
                 }
             }
-            // return this.nodeHasClick == false ? this.baseProject.children : (this.currentProject.children ? this.currentProject.children.slice(this.start,this.end) : []);
-            // return this.currentProject.children.slice(start,end);
         },
     },
     created() {//初始化数据
-        Axios({
-            headers: {
-                'token': this.$store.getters.getToken
-            },
-            url: '/baseProjects',
-            method: 'get',
-        }).then(Response => {
-            console.log(Response);
-            this.baseProject.children = Response.data.data;
-            this.parentNode = this.recursion(this.baseProject.children);
-        }).catch(error => {
-            console.log(error);
-        })
+        this.init();
     },
     methods: {
+        ensureUpdateProject(){
+            Axios({
+                headers:{
+                    'token': this.$store.getters.getToken
+                },
+                url: '/baseProject',
+                method: 'put',
+                data: {
+                    id: this.updateForm.id,
+                    name: this.updateForm.name,
+                    score: this.updateForm.score,
+                    level: this.updateForm.level ?  this.updateForm.level : null 
+                }
+            }).then(Response => {
+                if(Response.data.data == true){
+                    this.$message.success( '更新成功');
+                    this.init();
+                }else{
+                    this.$message.error('更新失败');
+                }
+            }).catch(error => {
+                this.$message.error('系统出现异常'+error);
+            })
+            this.updateProject = false;
+        },
+        addParentChange(value){
+            this.addForm.parentId = value[value.length - 1];
+        },
+        ensureAddProject(){
+            console.log('添加基础项目');
+            Axios({
+                headers:{
+                    'token': this.$store.getters.getToken
+                },
+                url: '/baseProject',
+                method: 'post',
+                data: {
+                    parentId: this.addForm.parentId,
+                    name: this.addForm.name,
+                    level: this.addForm.level,
+                    score: this.addForm.level
+                }
+            }).then(Response => {
+                if(Response.data.code == '000000'){
+                    this.$message.success( '添加成功');
+                }
+            }).catch(error => {
+                this.$message.error('出现异常'+error);
+            })
+            this.addProject = false;
+            this.init();
+        },
         recursion(project){ //递归删除空白子数组，避免级联选择器出现空白
-            console.log(project);
             for(var i = 0; i < project.length;i++){
                 if(project[i].children.length != 0){
                     this.recursion(project[i].children);
@@ -202,7 +277,7 @@ export default {
             }
             return project;
         },
-        concelAdd(){
+        cancelAdd(){
             this.addForm.parentProject = {};
             this.addForm.name = '';
             this.addForm.level = '';
@@ -218,6 +293,15 @@ export default {
             this.isSelect = false;
             this.projectName = "";
             this.filterText = "";
+            this.addForm.parentId = 0;
+            this.addForm.level = "";
+            this.addForm.name = "";
+            this.addForm.score = 0;
+            this.updateForm.id = 0;
+            this.updateForm.name = '';
+            this.updateForm.level = undefined;
+            this.updateForm.score = 0;
+
         },
         nodeClick(data){
             console.log(data);
@@ -226,13 +310,34 @@ export default {
         },
         modify(data){
             console.log(data);
+            this.updateForm.name = data.name;
+            this.updateForm.score = data.score;
+            this.updateForm.id = data.id;
+
+            console.log(this.updateForm);
+            this.updateProject = true;
         },
         add(){
-            console.log(this.parentNode);
             this.addProject = true;
         },
         deleted(data){
             console.log(data);
+            Axios({
+                headers:{
+                    'token': this.$store.getters.getToken
+                },
+                url: '/baseProject/'+data.id,
+                method: 'delete'
+            }).then(Response => {
+                if(Response.data.code == '000000'){
+                    this.$message.success( '删除成功')
+                    this.init();
+                }else{
+                    this.$message.error('删除失败')
+                }
+            }).catch(error => {
+                this.$message.error('出现异常'+error)
+            })
         },
         handleCurrentChange(val){
             this.currentPage = val;
@@ -243,6 +348,26 @@ export default {
         },
         select(){
             this.isSelect = true;
+        },
+        init(){
+            console.log('开始初始化');
+            this.reset();
+            Axios({
+                headers: {
+                    'token': this.$store.getters.getToken
+                },
+                url: '/baseProjects',
+                method: 'get',
+            }).then(Response => {
+                console.log(Response);
+                this.baseProject.children = Response.data.data;
+                this.parentNode = this.recursion(this.baseProject.children);
+            }).catch(error => {
+                this.$notify.error({
+                title: '错误',
+                message: '初始化数据异常'
+            });
+        })
         }
     },
     watch: {
